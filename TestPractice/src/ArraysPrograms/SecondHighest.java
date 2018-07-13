f15c9dfa5b1f66c7da04ac834ef7b7da5fe4c838
shriniwas.alle <shriniwas.alle@PUN-2CBXJC2.bcone.com>
2018-07-13 20:58:00 +0530

    New Programs added

--- TestPractice/src/ArraysPrograms/SecondHighest.java
+++ TestPractice/src/ArraysPrograms/SecondHighest.java
@@ -7,10 +7,9 @@ public class SecondHighest {
 	public static void main(String[] args) {
 		int arr[] = { 10, 5, 12, 15 };
 		Arrays.sort(arr);
-		for (int i = 0; i < arr.length; i++) {			
-			System.out.println(arr[arr.length-2]);
-			
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr.length);
+			// System.out.println(arr[arr.length-2]);
 		}
-	}
-
+	}	
 }
